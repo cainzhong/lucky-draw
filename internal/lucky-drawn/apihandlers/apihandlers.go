0f7bf1ac7a5895a65671837f061e935b837255f2
cainzhong <cainzhong@aliyun.com>
2019-04-16 21:55:42 +0800

    apihandlers.go

---
+++ internal/lucky-drawn/apihandlers/apihandlers.go
@@ -0,0 +1,10 @@
+package apihandlers
+
+import (
+	"fmt"
+	"github.com/kataras/iris"
+)
+
+func StartLuckyDraw(ctx iris.Context){
+	fmt.Println("*************************************** Start Lucky Draw ***************************************")
+}
